50975ed4a2aeb17d2bfdb674f9f49ce8a557b75c
Kristina Rozanova <zolina.kristina@gmail.com>
2023-09-14 21:45:26 +0300

    Initial commit

---
+++ src/Lesson1409.java
@@ -0,0 +1,17 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import java.util.Scanner;
+
+public class Lesson1409 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Say your name:");
+        String name = scanner.nextLine();
+        System.out.println("Hello," + name + "!");
+        System.out.println("Today is a nice weather!");
+    }
+}
